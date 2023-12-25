cc64bf2f29833a8a5cc94594c90ea453f3bdda06
Hrishabh Ayush <84783956+hrishabhayush@users.noreply.github.com>
2023-12-25 06:42:27 +0530

    Update main.py
    Add Twilio API features to send messages back to you whenever it will rain in Ithaca and hosted the code on PythonAnywhere

--- rain_alert/main.py
+++ rain_alert/main.py
@@ -1,11 +1,20 @@
 import requests
+import os
+from twilio.rest import Client
+from twilio.http.http_client import TwilioHttpClient
 
-API_KEY = '422a8f7f597508bec39bb048447046cc'
+API_KEY = 'your api key'
 LATTITUDE = 42.443962
 LONGITUDE = -76.501884
-#1ZZQJQNYXPEKV4QGX9YUHT2S
+URL = 'https://api.openweathermap.org/data/2.5/forecast'
 
-url = 'https://api.openweathermap.org/data/2.5/forecast'
+#1ZZQJQNYXPEKV4QGX9YUHT2S
+# Find your Account SID and Auth Token at twilio.com/console
+# and set the environment variables. See http://twil.io/secure
+account_sid = os.environ['TWILIO_ACCOUNT_SID']
+auth_token = os.environ['TWILIO_AUTH_TOKEN']
+
+
 params = {
     'lat' : LATTITUDE,
     'lon' : LONGITUDE,
@@ -13,7 +22,7 @@ params = {
     'cnt' : 4
 }
 
-response = requests.get(url, params = params)
+response = requests.get(URL, params = params)
 response.raise_for_status()
 weather_data = response.json()
 
@@ -25,4 +34,13 @@ for hour_data in weather_data['list']:
         will_rain = True
 
 if will_rain:
-    print('Bring an umbrella!') 
+    proxy_client = TwilioHttpClient()
+    proxy_client.session.proxies = {'https': os.environ['https_proxy']}
+    client = Client(account_sid, auth_token, http_client=proxy_client)
+    message = client.messages \
+                    .create(
+                        body="It's going to rain today. Remember to bring an umbrella.",
+                        from_='Trial number from Twilio',
+                        to='Your verified number'
+                    )
+#The number generated from Twilio goes under from and your number goes to to. 
